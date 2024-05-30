ddf99d86a043856ea8fc4e1a7dae286d06df6371
Nguyễn Đức Huy - PH33852 <139211478+duchuy112233@users.noreply.github.com>
2024-05-30 10:57:48 +0700

    30/5
    B1

---
+++ XBid_xuong_angular_nodejs/server/src/models/ProductModel.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const ProductSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+const Product = mongoose.model("Product", ProductSchema);
+
+export default Product;
